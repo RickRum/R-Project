ab22e37758684b96459f3ba91cce6e58932b451e
RickRum <rickrum@mail.ru>
2021-05-12 19:11:24 +0400

    admin_category_1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 	root "posts#index" , as: "home"
 	resources :posts
+	resources :profiles
 	namespace :admin do 
 		get '/', to: 'homes#index'
 		resources :posts
