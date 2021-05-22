7e40b0ab0b27cc72e62c05d37ccc1353984fae87
RickRum <rickrum@mail.ru>
2021-05-22 15:11:42 +0400

    profile_image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:category])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:category, :image])
   end
 end
  
